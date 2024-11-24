4e89acd230a24d8755ac7392c22ddd2bdec1668d
Nicholas H <739415@student.dusd.net>
2024-11-23 22:45:11 -0800

    health and speed
    added health and speed and more blocking movements

--- 2DBeginnerAdventureP8Horn/Assets/Scripts/PlayerMover.cs
+++ 2DBeginnerAdventureP8Horn/Assets/Scripts/PlayerMover.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth; 
+
+
     Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
@@ -12,6 +17,7 @@ public class PlayerMover : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -23,9 +29,14 @@ public class PlayerMover : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2D.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
